d81018c341a754fa745709c8ca4b1824c7ebfdfb
Hoti Fixe <hoti@qsuper.com.au>
2015-02-06 23:28:20 +1100

    forget

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ echo `git rev-parse HEAD` > source/version.html
 bundle exec middleman build
 cd build
 s3cmd sync --acl-public --guess-mime-type --recursive . s3://distributedlife.com/
+s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Cache-Control:public" s3://distributedlife.com/images/
 cd ..
 rm -rf build
