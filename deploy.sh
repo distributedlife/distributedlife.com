13e13d788cff0ae2c592ec224f8cca60d7424c59
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-02-26 22:10:32 +1000

    moved some common meta out of index; gzip and vary header on javascript and stylesheet

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ bundle exec middleman build
 cd build
 s3cmd sync --acl-public --guess-mime-type --recursive . s3://distributedlife.com/
 s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Vary: Accept-Encoding" --add-header="Cache-Control:public" --add-header='Content-Encoding: gzip' s3://distributedlife.com/images/
-s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Vary: Accept-Encoding" --add-header="Content-Type: utf-8" --add-header='Content-Encoding: gzip' s3://distributedlife.com/**/*.html
+s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Vary: Accept-Encoding" --add-header="Content-Type: utf-8" --add-header='Content-Encoding: gzip' s3://distributedlife.com/javascripts/
+s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Vary: Accept-Encoding" --add-header="Content-Type: utf-8" --add-header='Content-Encoding: gzip' s3://distributedlife.com/stylesheets/
 cd ..
 rm -rf build
