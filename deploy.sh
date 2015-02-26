5ebbfd3f278a549ce7055c946031c1a1dc4ea4c1
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-02-26 22:06:12 +1000

    get rid of unused JS; added moar headers for perf

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ echo `git rev-parse HEAD` > source/version.html
 bundle exec middleman build
 cd build
 s3cmd sync --acl-public --guess-mime-type --recursive . s3://distributedlife.com/
-s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Cache-Control:public" s3://distributedlife.com/images/
+s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Vary: Accept-Encoding" --add-header="Cache-Control:public" --add-header='Content-Encoding: gzip' s3://distributedlife.com/images/
+s3cmd modify --acl-public --guess-mime-type --recursive --add-header="Vary: Accept-Encoding" --add-header="Content-Type: utf-8" --add-header='Content-Encoding: gzip' s3://distributedlife.com/**/*.html
 cd ..
 rm -rf build
